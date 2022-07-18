52485225fd59fd0b2631c5433abb649326cf80e7
MoniMak95 <91891136+MoniMak95@users.noreply.github.com>
2022-07-18 17:51:34 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 
+// C++ recursive function to
+// solve tower of hanoi puzzle
+#include <bits/stdc++.h>
 using namespace std;
 
+void towerOfHanoi(int n, char from_rod,
+                    char to_rod, char aux_rod)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    cout << "Move disk " << n << " from rod " << from_rod <<
+                                " to rod " << to_rod << endl;
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+}
+
+// Driver code
 int main()
 {
-    int n, reversed_number = 0, remainder;
-
-  cout << "Enter an integer: ";
-  cin >> n;
-  while(n != 0) {
-    remainder = n % 10;
-    reversed_number = reversed_number * 10 + remainder;
-    n /= 10;
-  }
-
-  cout << "Reversed Number = " << reversed_number;
-
+    int n = 3; // Number of disks
+    towerOfHanoi(n, 'A', 'C', 'B'); // A, B and C are names of rods
     return 0;
 }
